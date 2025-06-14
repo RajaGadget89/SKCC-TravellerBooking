6f3e63ecb20645c73207799312d2d913aa03a7bc
RajaGadgets89 <rajagadgets89@gmail.com>
2025-06-15 00:09:29 +0700

    chore: remove legacy pages directory and obsolete test to fully migrate to App Router

--- src/__tests__/example.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Home from '@/pages/index'
-
-describe('Home', () => {
-  it('renders without crashing', () => {
-    render(<Home />)
-    // Add more specific tests as we develop the home page
-  })
-}) 
